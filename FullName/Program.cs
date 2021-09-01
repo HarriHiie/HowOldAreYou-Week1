e0121013bfe0f89410ee67db1ee4e4c6eca3012e
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:14:27 +0300

    read inputs

---
+++ FullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //step 1. programm küsib kasutaja käest kasutaja eesnime
+            //step 2. programm küsib kasutaja käest kasutaja perekonnanime
+            //step 3. programm tervitab kasutajat kasutades neid andmeid
+            Console.WriteLine("What Is Your Name?");
+            //Readline() to read the user input from the console
+            string UserName1 = Console.ReadLine();
+            Console.WriteLine("What Is Your Last Name?");
+            //Readline() to read the user input from the console
+            string UserName2 = Console.ReadLine();
+            Console.WriteLine("Hello," + UserName1 + UserName2 );
+        }
+    }
+}
---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Is Your Name?");
+            //Readline() to read the user input from the console
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Hello," + UserName);
+            Console.Read();
+            Console.WriteLine("How Old Are You? Enter digits only");
+            int Age = Int32.Parse(Console.ReadLine());
+            int YearOfBirth = 2021 - Age;
+            //Console.WriteLine("You Were Born In " + YearOfBirth);
+            Console.WriteLine($"You Were Born In {YearOfBirth}");
+
+        }
+    }
+}
